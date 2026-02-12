c5fc03316e74fe7f54074618e8353ba586795c3b
supalosa <supalosa@supalosa.com>
2026-02-12 16:12:01 +1100

    exclude dist/ tests

--- packages/chronodivide-bot-driver/vitest.config.ts
+++ packages/chronodivide-bot-driver/vitest.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'node',
-  },
+    test: {
+        globals: true,
+        environment: "node",
+        include: ["src/**/*.{test,spec}.{js,ts}"],
+        exclude: ["**/node_modules/**", "**/dist/**"],
+    },
 });
